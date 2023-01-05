db04c067542b453eedb69c0bbb3c90c976422840
91994 <suddala.rajukumar123@gmail.com>
2023-01-05 10:46:44 +0530

    updating

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,18 +1,19 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ami" "ec2_instance" {
-  most_recent = true
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["${data.aws_caller_identity.current.account_id}"]
-}
-
+#data "aws_caller_identity" "current" {}
+#
+#data "aws_ami" "ec2_instance" {
+#  most_recent = true
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["${data.aws_caller_identity.current.account_id}"]
+#}
+#
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ec2_instance.id}"
+#  ami           = "${data.aws_ami.ec2_instance.id}"
+  ami= "ami-0574da719dca65348"
   instance_type = "t2.micro"
 
   tags = {
